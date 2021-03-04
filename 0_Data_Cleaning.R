0247c9bd5a67be708f0894bf1d2a2ddfaf7b0159
Luisa Balzus <luisa.balzus@gmail.com>
2021-03-04 16:42:18 +0100

    Update ERN area amplitude quantification and related analyses

--- 0_Data_Cleaning.R
+++ 0_Data_Cleaning.R
@@ -46,7 +46,6 @@ for (participant in logfiles){
 # Load ERP data
 ERN_data     <- read.csv(paste0(path_ERP_data, "single_trial_MFN_0_100_FCz_with_events.csv"),             header = TRUE, stringsAsFactors = FALSE)
 ERN_baseline <- read.csv(paste0(path_ERP_data, "single_trial_MFN_baseline_-200_0_FCz_with_events.csv"),   header = TRUE, stringsAsFactors = FALSE)
-ERN_area     <- read.csv(paste0(path_ERP_data, "single_trial_MFN_mean_area_-50_150_FCz_with_events.csv"), header = TRUE, stringsAsFactors = FALSE)
 Pe_data      <- read.csv(paste0(path_ERP_data, "single_trial_Pe_200_400_Pz_with_events.csv"),             header = TRUE, stringsAsFactors = FALSE)
 Pe_baseline  <- read.csv(paste0(path_ERP_data, "single_trial_Pe_baseline_-200_0_Pz_with_events.csv"),     header = TRUE, stringsAsFactors = FALSE)
 
@@ -76,7 +75,6 @@ behavioral_data$name <- gsub('Flanker_', '', behavioral_data$name)
 # Fix missing trials (no trigger sent, e.g. due to RT = 1 ms triggers may overlap and only one is sent) in ERP data (increment trial nummer after missing trial by number of missing trials to enable matching)
 ERN_data[ERN_data$participant_id == 'P_17_T1'         & ERN_data$trial >= 270,]$trial     <- ERN_data[ERN_data$participant_id == 'P_17_T1'         & ERN_data$trial >= 270,]$trial     + 1 # in P_17_T1 trial 270 is missing 
 ERN_baseline[ERN_baseline$participant_id == 'P_17_T1' & ERN_baseline$trial >= 270,]$trial <- ERN_baseline[ERN_baseline$participant_id == 'P_17_T1' & ERN_baseline$trial >= 270,]$trial + 1 # in P_17_T1 trial 270 is missing 
-ERN_area[ERN_area$participant_id == 'P_17_T1'         & ERN_area$trial >= 270,]$trial     <- ERN_area[ERN_area$participant_id == 'P_17_T1'         & ERN_area$trial >= 270,]$trial     + 1 # in P_17_T1 trial 270 is missing 
 Pe_data[Pe_data$participant_id == 'P_17_T1'           & Pe_data$trial >= 270,]$trial      <- Pe_data[Pe_data$participant_id == 'P_17_T1'           & Pe_data$trial >= 270,]$trial      + 1 # in P_17_T1 trial 270 is missing 
 Pe_baseline[Pe_baseline$participant_id == 'P_17_T1'   & Pe_baseline$trial >= 270,]$trial  <- Pe_baseline[Pe_baseline$participant_id == 'P_17_T1'   & Pe_baseline$trial >= 270,]$trial  + 1 # in P_17_T1 trial 270 is missing 
 
@@ -87,9 +85,8 @@ Pe_baseline[Pe_baseline$participant_id == 'P_17_T1'   & Pe_baseline$trial >= 270
 
 
 # Merge behavioral and ERP data (rows in behavioral_data with no match in ERN_data will have NAs in the ERP column)
-single_trial_data <- left_join(behavioral_data, ERN_data, by = c('name' = 'participant_id', 'trial' = 'trial', 'resp1' = 'event'))
+single_trial_data <- left_join(behavioral_data,   ERN_data, by = c('name' = 'participant_id', 'trial' = 'trial', 'resp1' = 'event'))
 single_trial_data <- left_join(single_trial_data, ERN_baseline, by = c('name' = 'participant_id', 'trial' = 'trial', 'resp1' = 'event', 'artifact' = 'artifact'))
-single_trial_data <- left_join(single_trial_data, ERN_area, by = c('name' = 'participant_id', 'trial' = 'trial', 'resp1' = 'event', 'artifact' = 'artifact'))
 single_trial_data <- left_join(single_trial_data, Pe_data, by = c('name' = 'participant_id', 'trial' = 'trial', 'resp1' = 'event', 'artifact' = 'artifact'))
 single_trial_data <- left_join(single_trial_data, Pe_baseline, by = c('name' = 'participant_id', 'trial' = 'trial', 'resp1' = 'event', 'artifact' = 'artifact'))
 
@@ -133,12 +130,11 @@ single_trial_data[single_trial_data$name =="C_01_T2"| single_trial_data$name =="
 
 # Rename columns and remove string '_T1/T2' from participant ID (to get correct number of factor levels later)
 single_trial_data <- single_trial_data %>% 
-  subset(select = c("name", "group", "session", "stimulation", "trial", "stimulus_type", "response_type", "rt1", "rt_log", "rt_invalid" ,"response_type_2nd", "rt2", "MFN_0_100_FCz", "MFN_.200_0_FCz", "MFN_.50_150_FCz", "Pe_200_400_Pz", "Pe_.200_0_Pz")) %>%
+  subset(select = c("name", "group", "session", "stimulation", "trial", "stimulus_type", "response_type", "rt1", "rt_log", "rt_invalid" ,"response_type_2nd", "rt2", "MFN_0_100_FCz", "MFN_.200_0_FCz", "Pe_200_400_Pz", "Pe_.200_0_Pz")) %>%
   dplyr::rename(participant_id     = name,
                 rt                 = rt1,
                 rt_2nd             = rt2,
                 MFN_baseline_pre_200_0_FCz = MFN_.200_0_FCz,
-                MFN_area_pre_50_150_FCz    = MFN_.50_150_FCz,
                 Pe_baseline_pre_200_0_Pz   = Pe_.200_0_Pz) %>% 
   dplyr::mutate(participant_id = substr(participant_id, 1, 4))
 
